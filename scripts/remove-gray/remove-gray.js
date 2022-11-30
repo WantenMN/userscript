a1825382733e4cd172002b814981b84bf6f2402f
Wanten <WantenMN@gmail.com>
2022-12-01 02:06:55 +0800

    update

--- scripts/remove-gray/remove-gray.js
+++ scripts/remove-gray/remove-gray.js
@@ -1,17 +1,19 @@
 // ==UserScript==
 // @name         黑白首页，灰白首页，黑色首页去除(Remove gray | Remove filter)
 // @namespace    https://greasyfork.org/en/scripts/455710
-// @version      0.0.1
+// @version      0.0.2
 // @description  移除部分网站首页的灰白效果。
 // @author       Wanten
 // @copyright    2022 Wanten
 // @supportURL   https://github.com/WantenMN/userscript/issues
 // @license      GNU General Public License v3.0
 // @match        *://*/*
+// @run-at       document-start
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
-// @grant        none
+// @grant        GM_addStyle
 // ==/UserScript==
 
 (function() {
-  document.documentElement.style.filter = "none";
+  //document.documentElement.style.filter = "none";
+  GM_addStyle("*{-webkit-filter: none !important;filter:  none !important;}");
 })();
